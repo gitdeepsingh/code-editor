a83adf0c2471c971fba46f670f213aa5ef87be73
deepsingh <deepsingh.cts@gmail.com>
2021-12-19 21:18:48 +0530

    editor and viwer

--- src/App.js
+++ src/App.js
@@ -1,17 +1,40 @@
+import React from "react";
 import SplitPane from "react-split-pane";
+import ReactMarkdown from "react-markdown";
+import Editor from "./editor.js";
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <SplitPane split="vertical" defaultSize="50%">
-        <div className="editor-pane">
-        </div>
-        <div className="view-pane">
-        </div>
-      </SplitPane>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super();
+
+    this.state = {
+      markdownSrc: "# Hello World",
+    }
+    this.onMarkdownChange = this.onMarkdownChange.bind(this);
+  }
+
+  onMarkdownChange(md) {
+    this.setState({
+      markdownSrc: md
+    });
+  }
+
+  render() {
+    return (
+      <div className="App" >
+        <SplitPane split="vertical" defaultSize="50%">
+          <div className="editor-pane">
+            <Editor className="editor" value={this.state.markdownSrc} onChange={this.onMarkdownChange} />
+          </div>
+          <div className="view-pane">
+            <ReactMarkdown className="result" children={this.state.markdownSrc} />
+          </div>
+        </SplitPane>
+      </div>
+    );
+  }
 }
 
 export default App;
